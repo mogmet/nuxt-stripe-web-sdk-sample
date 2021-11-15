f0ea7e5b78b764a4b66d6d67051f98bd14358892
mogmet <mogmet@gmail.com>
2021-11-15 14:18:09 +0900

    [feat] コメント追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <v-row>
+    <!-- 課金済みは現在課金している情報を表示 -->
     <v-col v-if='this.subscriptions.length > 0'>
       <v-card v-for='subscript in subscriptions' :key='subscript.id'>
         <v-card-title>{{getProductTitle(subscript.product)}}</v-card-title>
         <v-card-text>{{subscript.current_period_start}} - {{subscript.current_period_end}}</v-card-text>
       </v-card>
     </v-col>
+    <!-- 商品一覧を表示 -->
     <v-col v-for="product in products" v-else :key="product.id">
       <v-img :src='product.images[0]' height='240' aspect-ratio='3' />
       <v-card :loading='isLoading'>
@@ -28,10 +30,9 @@ import {
   onCurrentUserSubscriptionUpdate,
   // eslint-disable-next-line import/named
   Product,
-// eslint-disable-next-line import/named
+  // eslint-disable-next-line import/named
   Subscription
 } from '@stripe/firestore-stripe-payments'
-// import { Context } from '@nuxt/types'
 import Vue from 'vue'
 import { signInAnonymously } from 'firebase/auth'
 import { payments } from '~/stripe'
@@ -54,16 +55,19 @@ export default Vue.extend({
   },
   async created() {
     await signInAnonymously(auth)
+    // 商品一覧を取得
     const products = await getProducts(payments, {
       includePrices: true,
       activeOnly: true,
     })
     this.products = products
+    // 既に課金済みであればサブスク情報をセットする
     onCurrentUserSubscriptionUpdate(payments, snapshot => {
       this.subscriptions = snapshot.changes.map(value => value.subscription)
     })
   },
   methods: {
+    /// サブスク購入処理
     async onClickPurchase(priceId: string): Promise<void> {
       this.isLoading = true
       try {
@@ -79,6 +83,7 @@ export default Vue.extend({
         this.isLoading = false
       }
     },
+    /// 商品名を返す
     getProductTitle(productId: string): string {
       return this.products.find(product => product.id === productId)?.name ?? ''
     }
